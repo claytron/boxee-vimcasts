8a57033ea17fab813252576cab2ab33e1b442fc8
Clayton Parker <clayton@sixfeetup.com>
2010-06-28 00:02:11 -0400

    Added new helper named cleanse_feed
    - remove non video items
    - Add in the episode number
    
    This fixes #1 and #2

--- helpers.py
+++ helpers.py
@@ -11,3 +11,25 @@ def play_video():
     if player.IsPlaying():
         player.Stop()
     player.Play(shows[focused_item])
+
+
+def cleanse_feed():
+    """Modify the feed to do two things.
+
+    - Make sure that non video items don't show up in the UI.
+    - Add the episode number to the "label"
+    """
+    feed_list = mc.GetWindow(14000).GetList(100)
+    keepers = mc.ListItems()
+    for episode in feed_list.GetItems():
+        video_base = "http://media.vimcasts.org/videos/"
+        ep_path = episode.GetPath()
+        ep_title = episode.GetLabel()
+        if ep_path.startswith(video_base):
+            ep_number = ep_path.split("/")[4]
+            episode.SetLabel("#%s - %s" % (ep_number, ep_title))
+            keepers.append(episode)
+        else:
+            mc.LogDebug("vimcasts removing non video item: %s" % ep_path)
+    # reset the list to only the items that contained a video
+    feed_list.SetItems(keepers)
