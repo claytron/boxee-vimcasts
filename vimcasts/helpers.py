0aa816cb9e822827634646f2d123dc50a240831b
Clayton Parker <clayton@sixfeetup.com>
2010-07-03 10:25:00 -0400

    remove override of video play so users can use the share/queue buttons

--- vimcasts/helpers.py
+++ vimcasts/helpers.py
@@ -1,17 +1,5 @@
 import mc
 
-def play_video():
-    """Play the selected video directly instead of asking the user
-    if they want to play it.
-    """
-    player = mc.GetPlayer()
-    feed_list = mc.GetWindow(14000).GetList(100)
-    focused_item = feed_list.GetFocusedItem()
-    episodes = feed_list.GetItems()
-    if player.IsPlaying():
-        player.Stop()
-    player.Play(episodes[focused_item])
-
 
 def cleanse_feed():
     """Modify the feed to do two things.
